98d893784da53c6ba7d05373da314cac6ee5f475
Fernicita <Fernanda.arellanes1600@gmail.com>
2023-05-16 17:26:46 -0600

    Server
    Los datos del server

---
+++ app.js
@@ -0,0 +1,64 @@
+const  http =   require ( 'http' ) ;
+const fs = require('fs');
+
+http.createServer((request, response)=>{
+    const file = request.url == '/' ? './WWW/index.html' : `./WWW${request.url}`;
+
+    if(request.url == "/login"){
+        let data = [];
+        request.on("data", value => {
+            data.push(value);
+
+        }).on("end", () => {    
+            let params =Buffer.concat(data).toString();
+            response.write(params);
+            response.end();
+        })
+    }
+    
+    fs.readFile(file, (err, data)=>{
+        if(err){
+            response.writeHead(404, {"Content-Type": "text/plain"});
+            response.write("404 No lo encontre perdon toy chikita");
+            response.end();
+        }
+        else{
+            
+            const extension = request.url.split('.').pop
+            switch (extension) {
+                case "txt":
+                    response.writeHead(200, {"Content-Type": "text/plain"});
+                    response.write(data);
+                    response.end();
+                    break;
+                case "html":
+                    response.writeHead(200, {"Content-Type": "text/html"});
+                    break;
+                case "css":
+                    response.writeHead(200, {"Content-Type": "text/css"});
+                    break;
+                case "ico":
+                    response.writeHead(200, {"Content-Type": "image/x-icon"})
+                    break;
+                case "js":
+                    response.writeHead(200, {"Content-Type": "text/javascript"})
+                    break;
+                case "jpeg":
+                    response.writeHead(200, {"Content-Type": "image/jpeg"})
+
+                default:
+                    response.writeHead(200, {"Content-Type": "text/html"});
+                    break;
+            }
+            
+            response.write(data);
+            response.end();
+    
+
+        }
+       
+
+    })
+    
+}).listen(4444);
+
